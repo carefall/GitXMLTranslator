2c98fa40506933d469106bf9488eb6840d19df1b
Ivan <carefall.regular@gmail.com>
2026-07-04 22:46:17 +0300

    Basic setup

---
+++ GitWindowXML.xaml.cs
@@ -0,0 +1,49 @@
+﻿using GitXMLTranslator.Internals;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace GitXMLTranslator
+{
+
+    public partial class GitWindowXML : Window
+    {
+
+        private static readonly Regex _regex = new(@"^[\p{L}\p{Nd}\s.,-]*$", RegexOptions.Compiled);
+
+        public GitWindowXML()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ContinueButton.IsEnabled = false;
+            Settings s = new();
+            int result = await GitHubClient.Check(s.gamedataPath);
+            //
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (sender is not TextBox textBox)
+                return;
+            if (!_regex.IsMatch(textBox.Text))
+            {
+                int caret = textBox.CaretIndex;
+                if (caret > 0)
+                {
+                    textBox.Text = textBox.Text.Remove(caret - 1, 1);
+                    textBox.CaretIndex = caret - 1;
+                }
+            }
+            ContinueButton.IsEnabled = textBox.Text.Length > 2;
+            
+
+            
+        }
+    }
+}
---
+++ Internals/Settings.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Text.Json;
+using System.Windows;
+
+namespace GitXMLTranslator.Internals
+{
+    internal class Settings
+    {
+
+        public string gamedataPath = "";
+        JsonSerializerOptions options = new()
+        {
+            WriteIndented = true
+        };
+
+        public Settings() {
+            try
+            {
+                string folderPath = AppDomain.CurrentDomain.BaseDirectory + "/Assets";
+                string settingsPath = AppDomain.CurrentDomain.BaseDirectory + "/Assets/settings.json";
+                if (!File.Exists(settingsPath))
+                {
+                    Directory.CreateDirectory(folderPath);
+                    //File.Create(settingsPath).Close();
+                    var config = new Dictionary<string, string>
+                    {
+                        ["gamedata-path"] = ""
+                    };
+                    string data = JsonSerializer.Serialize(config, options);
+                    File.WriteAllText(settingsPath, data);
+                }
+                string json = File.ReadAllText(settingsPath);
+                using JsonDocument doc = JsonDocument.Parse(json);
+                var root = doc.RootElement;
+                var property = root.GetProperty("gamedata-path");
+                string? value = property.GetString();
+                if (value == null || value.Trim().Length == 0)
+                {
+                    var dialog = new OpenFolderDialog
+                    {
+                        Title = "Выберите папку с gamedata",
+                        InitialDirectory = @"C:\"
+                    };
+                    while (dialog.ShowDialog() != true)
+                    {
+                        var result = MessageBox.Show("Выберите папку, куда будут размещены файлы(папка gamedata и её содержимое)", "Настройка", MessageBoxButton.YesNo);
+                        if (result == MessageBoxResult.No) Application.Current.Shutdown();
+                    }
+                    string path = dialog.FolderName;
+                    gamedataPath = path;
+                    var config = new Dictionary<string, string>
+                    {
+                        ["gamedata-path"] = path
+                    };
+                    string data = JsonSerializer.Serialize(config, options);
+                    File.WriteAllText(settingsPath, data);
+                    return;
+                }
+                gamedataPath = value;
+            } catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                Application.Current.Shutdown();
+            }
+        }
+
+    }
+}
